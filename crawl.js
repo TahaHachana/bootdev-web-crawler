b3e4926e54df0890d7b7f4a1c5750939dbf212b8
TahaHachana <tahahachana@gmail.com>
2024-06-28 07:26:45 +0100

    normalizeURL tests

--- crawl.js
+++ crawl.js
@@ -3,3 +3,4 @@ function normalizeURL(url) {
 }
 
 export { normalizeURL };
+
--- crawl.test.js
+++ crawl.test.js
@@ -1,3 +1,36 @@
 import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
 
+test("HTTPS and trailing slash", () => {
+  expect(normalizeURL("https://blog.boot.dev/path/")).toBe(
+    "blog.boot.dev/path"
+  );
+});
+
+test("HTTPS and no trailing slash", () => {
+  expect(normalizeURL("https://blog.boot.dev/path")).toBe(
+    "blog.boot.dev/path"
+  );
+});
+
+test("HTTP and trailing slash", () => {
+  expect(normalizeURL("http://blog.boot.dev/path/")).toBe(
+    "blog.boot.dev/path"
+  );
+});
+
+test("HTTP and no trailing slash", () => {
+  expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+});
+
+test("Contains query", () => {
+  expect(normalizeURL("https://blog.boot.dev/path?id=123")).toBe(
+    "blog.boot.dev/path"
+  );
+});
+
+test("Contains fragment", () => {
+  expect(normalizeURL("https://blog.boot.dev/path#123")).toBe(
+    "blog.boot.dev/path"
+  );
+});
