45b84ae0b26f052a567d631b152d8de87fe2a8b2
TahaHachana <tahahachana@gmail.com>
2024-07-01 05:36:33 +0100

    Parse links with JSDOM

--- crawl.js
+++ crawl.js
@@ -1,6 +1,16 @@
+import { JSDOM } from "jsdom";
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
   return urlObj.host + urlObj.pathname.replace(/\/$/, "");
 }
 
-export { normalizeURL };
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  const urls = Array.from(dom.window.document.querySelectorAll("a")).map(
+    (a) => a.href
+  );
+  return urls.map((url) => new URL(url, baseURL).href);
+}
+
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
 
+// Test cases for normalizeURL
 test("HTTPS and trailing slash", () => {
   expect(normalizeURL("https://blog.boot.dev/path/")).toBe(
     "blog.boot.dev/path"
@@ -8,15 +9,11 @@ test("HTTPS and trailing slash", () => {
 });
 
 test("HTTPS and no trailing slash", () => {
-  expect(normalizeURL("https://blog.boot.dev/path")).toBe(
-    "blog.boot.dev/path"
-  );
+  expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path");
 });
 
 test("HTTP and trailing slash", () => {
-  expect(normalizeURL("http://blog.boot.dev/path/")).toBe(
-    "blog.boot.dev/path"
-  );
+  expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
 });
 
 test("HTTP and no trailing slash", () => {
@@ -33,4 +30,69 @@ test("Contains fragment", () => {
   expect(normalizeURL("https://blog.boot.dev/path#123")).toBe(
     "blog.boot.dev/path"
   );
+});
+
+// Test cases for getURLsFromHTML
+test("Single link", () => {
+  const htmlBody = `<a href="/path">Link</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path",
+  ]);
+});
+
+test("Multiple links", () => {
+  const htmlBody = `<a href="/path1">Link 1</a><a href="/path2">Link 2</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2",
+  ]);
+});
+
+test("No links", () => {
+  const htmlBody = `<div>No links</div>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([]);
+});
+
+test("Absolute links", () => {
+  const htmlBody = `<a href="https://blog.boot.dev/path">Link</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path",
+  ]);
+});
+
+test("Relative links", () => {
+  const htmlBody = `<a href="path">Link</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path",
+  ]);
+});
+
+test("Mixed links", () => {
+  const htmlBody = `<a href="https://blog.boot.dev/path1">Link 1</a><a href="/path2">Link 2</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2",
+  ]);
+});
+
+test("Links with query", () => {
+  const htmlBody = `<a href="/path?id=123">Link</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path?id=123",
+  ]);
+});
+
+test("Links with fragment", () => {
+  const htmlBody = `<a href="/path#123">Link</a>`;
+  const baseURL = "https://blog.boot.dev";
+  expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([
+    "https://blog.boot.dev/path#123",
+  ]);
 });
